6abefd92ba49a174d31600dd5de8e126a549983e
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-03-02 19:31:48 -0700

    styled navbar

--- src/main-app/Components/Navbar.tsx
+++ src/main-app/Components/Navbar.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
+import { FaGithub } from "react-icons/fa"
 import styles from "../Styles/Navbar.module.css";
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-        <li className={`${styles.navLeft}`}><a href="/" className={styles.navLink}>Home</a></li>
-        <li className={`${styles.navRight}`}><a href="https://github.com/your-github-username/your-repository-name" className={styles.navLink}>Github</a></li>
+        <li className={`${styles.navLeft}`}>
+          <a href="/" className={styles.navLink}>
+            <img src="autosteganologo.png" className={styles.logo}/>
+            <p className={styles.logo_text}><span>Auto</span>Stegano</p>
+          </a>
+        </li>
+        <li className={`${styles.navRight}`}>
+          <a href="https://github.com/your-github-username/your-repository-name" className={styles.navLink}>
+            <FaGithub className={styles.github_icon}/>
+          </a>
+        </li>
       </ul>
     </nav>
   );
